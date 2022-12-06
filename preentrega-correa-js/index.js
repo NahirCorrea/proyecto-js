07b28f6ccdf146b1bfa22f995e9852cbbe5e55a3
Nahir Correa <114889475+NahirCorrea@users.noreply.github.com>
2022-12-06 18:34:08 -0300

    Add files via upload

---
+++ preentrega-correa-js/index.js
@@ -0,0 +1,52 @@
+let nombreDelUser = prompt ( "Ingrese su Nombre de Usuario" )
+
+console.log ( nombreDelUser )
+
+let notaDelUser = prompt ( "Deja tu comentario" )
+
+console.log ( notaDelUser.length)
+
+if ( notaDelUser.length < "30" ) {
+
+    } else if ( alert ( "Esta entrada excede la cantidad de caracteres disponibles. ") ) {
+
+    } else {  alert ( nombreDelUser  + notaDelUser ) 
+
+}
+
+
+
+
+
+let solicAmistad = ` Juan Manuel Rojas ` 
+
+let solicAmistad1 = `Maria Julia Valverde `
+
+let solicAmistad2 = ` Joaquina Fernandez `
+
+
+
+for ( let i = 0 ; i <= 2  ; i++ ) {
+
+    confirm ( `El usuario ${ solicAmistad } te envio una solicitud de amistad` )
+
+} if ( solicAmistad <= 3 ) {
+
+    confirm ( `El usuario ${ solicAmistad1 } te envio una solicitud de amistad` ) 
+
+} else if ( solicAmistad1 <= 3 ) {
+
+    confirm ( `El usuario ${ solicAmistad2 } te envio una solicitud de amistad` ) 
+}
+
+
+
+
+
+
+
+
+
+
+
+
